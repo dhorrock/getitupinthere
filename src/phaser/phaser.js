5e20bfc0d84133cbf6aba6ea9ba2908303f9a46c
Travis Scott <traviss@iqmetrix.com>
2017-10-29 00:32:09 -0500

    broken

--- src/phaser/phaser.js
+++ src/phaser/phaser.js
@@ -1,34 +1,58 @@
-import phaser from "phaser";
+import Phaser from "phaser";
 import logo from "../assets/logo.png";
 
+var baseGameSceneConfig = {
+  preload: preload,
+  create: create,
+  key: 'base'
+};
+
+var backgroundSceneConfig = {
+    key: 'background',
+    active: true,
+    create: createBackground,
+    render: renderBackground,
+    files: [
+        { type: 'image', key: 'backgroundImage', url: '../assets/background.png' }
+    ]
+};
+
 var config = {
-  type: phaser.AUTO,
+  type: Phaser.AUTO,
   parent: "phaser-example",
   width: 800,
   height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+  scene: [backgroundSceneConfig, baseGameSceneConfig]
 };
 
-let game = new phaser.Game(config);
+function createBackground() {
+  this.add.image(0, 0, 'backgroundImage')
+}
+
+function renderBackground() {
+
+}
+
+let game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logo);
+ 
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+const rect = new Phaser.Geom.Rectangle(32, 32, 128, 256);
+
+  // const logo = this.add.image(400, 150, "logo");
+
+  // this.tweens.add({
+  //   targets: logo,
+  //   y: 450,
+  //   duration: 2000,
+  //   ease: "Power2",
+  //   yoyo: true,
+  //   loop: -1
+  // });
 }
 
 export { game };
